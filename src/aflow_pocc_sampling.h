cd1ffcf5d15ab0cf22e4503a7bf2786d21cd1e31
Yuxiang Liu <liuyuxiang92@gmail.com>
2024-04-20 17:45:38 -0400

    Change the name of first_random_seed_sampling  to first_random_sampling

--- src/aflow_pocc_sampling.h
+++ src/aflow_pocc_sampling.h
@@ -3,7 +3,7 @@
 #ifndef _AFLOW_POCC_SAMPLING_H_
 #define _AFLOW_POCC_SAMPLING_H_
 namespace pocc {
-    vector<unsigned long long int> first_random_seed_sampling(unsigned long long int hnf_count, unsigned long long int types_config_permutations_count);
+    vector<unsigned long long int> first_random_sampling(unsigned long long int hnf_count, unsigned long long int types_config_permutations_count);
     std::list<pocc::POccSuperCellSet> second_random_seed_sampling(std::list<pocc::POccSuperCellSet> l_supercell_sets, unsigned long long int hnf_count);
     double setPOccSampleRate(const string& pocc_sample_rate_string, int sample_round);   
 }
