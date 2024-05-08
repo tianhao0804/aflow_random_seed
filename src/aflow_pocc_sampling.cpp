3b9f656092e17cef8b63da40404cbb8ad87de7dd
Yuxiang Liu <liuyuxiang92@gmail.com>
2024-05-08 11:02:30 -0400

    Fix the aflow_pocc_sampling.cpp head star format

--- src/aflow_pocc_sampling.cpp
+++ src/aflow_pocc_sampling.cpp
@@ -1,8 +1,8 @@
 // ***************************************************************************
-// * *
-// * AFlow COREY OSES Johns Hopkins University 2012-2024 *
-// * AFlow Yuxiang Liu Johns Hopkins University 2024 *
-// * *
+// *                                                                         *
+// *       AFlow COREY OSES Johns Hopkins University 2012-2024               *
+// *       AFlow Yuxiang Liu Johns Hopkins University 2024                   *
+// *                                                                         *                         
 // ***************************************************************************
 //aflow_pocc_sampling.cpp
 //Reading Pocc sampling rate from --pocc_sample_rate and --pocc_sample_number
